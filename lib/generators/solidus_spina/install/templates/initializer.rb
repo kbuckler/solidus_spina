39f148b21804c034f41c1c7d9d745a6ddc8271f7
Kenny Buckler <kbuckler@gmail.com>
2021-05-19 14:52:53 -0700

    Register SolidusSpina::Parts::SolidusProduct; remove ProductReadonly

--- lib/generators/solidus_spina/install/templates/initializer.rb
+++ lib/generators/solidus_spina/install/templates/initializer.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-SolidusSpina.configure do |config|
-
-end
-
-Spina::Part.register(Spina::Parts::SolidusRelatedProduct)
-Spina::Part.register(Spina::Parts::SolidusProductReadonly)
-Spina::Part.register(Spina::Parts::SolidusRelatedProductsRepeater)
+::Spina::Part.register(Spina::Parts::SolidusProduct)
+::Spina::Part.register(Spina::Parts::SolidusRelatedProduct)
+::Spina::Part.register(Spina::Parts::SolidusRelatedProductsRepeater)
