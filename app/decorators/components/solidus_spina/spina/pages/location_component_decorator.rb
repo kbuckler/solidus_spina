caf75b46a4624f00bc378f2f23281b4ae5f20e7a
Kenny Buckler <kbuckler@gmail.com>
2021-05-27 16:25:05 -0700

    Only show Solidus resource on Solidus pages

---
+++ app/decorators/components/solidus_spina/spina/pages/location_component_decorator.rb
@@ -0,0 +1,22 @@
+module SolidusSpina
+  module Spina
+    module Pages
+      module LocationComponentDecorator
+        def resource_scope
+          scope = ::Spina::Resource.all
+          scope = scope.where.not(name: SolidusSpina.config.spina_resource_name) unless @page.solidus_page?
+          scope.order(:label)
+        end
+
+        def resources
+          [main_collection_option] + resource_scope.map do |resource|
+            [resource.label, resource.id, { data: {
+              parent_pages_url:  helpers.spina.admin_parent_pages_path(resource_id: resource.id)
+            } }]
+          end
+        end
+      end
+      ::Spina::Pages::LocationComponent.prepend LocationComponentDecorator
+    end
+  end
+end
---
+++ app/decorators/models/solidus_spina/spina/theme_decorator.rb
@@ -0,0 +1,17 @@
+module SolidusSpina
+  module Spina
+    module ThemeDecorator
+      def new_page_templates(recommended: "")
+        templates = super(recommended: recommended)
+
+        show_template = (recommended == SolidusSpina.config.spina_view_template_name)
+
+        templates.send(show_template ? :select : :reject) do |template|
+          template.name == SolidusSpina.config.spina_view_template_name
+        end
+      end
+    end
+
+    ::Spina::Theme.prepend ThemeDecorator
+  end
+end
